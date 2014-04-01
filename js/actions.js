5779c1698fa225d0257860f518205a8f2363bc05
Gerson Yarce Franco <gerson.yarce@nuevosmedios.ws>
2014-04-01 08:47:06 -0500

    Fix

--- js/actions.js
+++ js/actions.js
@@ -4,11 +4,7 @@ var SEND_DREAM = "http://app.preparame.co/preparame/json_save_goal/";
 
 function days_to_test(){
 	var d1 = new Date(); //"now"
-<<<<<<< HEAD
 	var d2 = new Date("2014/08/03");  // some date
-=======
-	var d2 = new Date("2014/04/06");  // some date
->>>>>>> master
 	var diff = Math.abs(d1-d2); 
 	var one_day = 1000*60*60*24;
 	var days_left = Math.round(diff/one_day);
